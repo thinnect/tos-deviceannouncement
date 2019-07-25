b54feec601c50237240f837988fbd990ab82c33e
Raido Pahtma <raidopahtma@gmail.com>
2019-07-25 09:14:27 +0300

    Add feature list tests.

--- test/test.c
+++ test/test.c
@@ -247,22 +247,62 @@ comms_error_t fake_comms_send3(comms_layer_iface_t* comms, comms_msg_t* msg, com
 	debugb1("send3", payload, length);
 	packets_sent++;
 
-	uint8_t ref[] =
-	"\x01\x02" // hdr-version
-	"\x88\x77\x66\x55\x44\x33\x22\x11" // EUI64
-	"\x00\x00\x00\x01" // boot_number
-	"\xec\xee\x9d\xf7\x73\x6b\x4c\x10\xaf\xed\xde\x06\x05\xe5\x53\xf3" // Platform UUID
-	"\x08\x09\x0a" // HW version
-	"\x22\xb9\x39\x35\xd6\x30\x47\xe2\xb5\xfc\x30\xb3\xc7\x2b\xae\x5a" // Manufacturer UUID
-	"\x00\x00\x00\x00\x5d\x35\xd0\x2f" // production timestamp
-	"\x01\x02\x03\x04\x05\x06\x07\x08" // IDENT_TIMESTAMP
-	"\x01\x02\x03" // firmware version
-	;
-/*
-	if(memcmp(ref, payload, length) != 0) {
-		err1("payload mismatch");
-		test_errors++;
-	}*/
+	if(packets_sent == 1) {
+		uint8_t ref[] =
+		"\x00\x02" // hdr-version
+		"\x88\x77\x66\x55\x44\x33\x22\x11" // EUI64
+		"\x00\x00\x00\x01" // boot_number
+		"\x00\x00\x00\x00\x00\x0f\x42\x40" // boot_time
+		"\x00\x00\x00\x06" // uptime
+		"\x00\x00\x00\x6a" // lifetime
+		"\x00\x00\x00\x00" // announcement
+		"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" // UUID
+		"\x55" // position type
+		"\x00\x00\x00\x00" // lat
+		"\x00\x00\x00\x00" // lon
+		"\x00\x00\x00\x00" // elevation
+		"\x01\x00" // radio tech+channel
+		"\x01\x02\x03\x04\x05\x06\x07\x08" // IDENT_TIMESTAMP
+		"\x00\x00\x06\xd8" // feature hash
+		;
+		if(memcmp(ref, payload, length) != 0) {
+			err1("payload mismatch");
+			test_errors++;
+		}
+	}
+
+	if(packets_sent == 2) {
+		uint8_t ref[] =
+		"\x02\x02" // hdr-version
+		"\x88\x77\x66\x55\x44\x33\x22\x11" // EUI64
+		"\x00\x00\x00\x01" // boot_number
+		"\x03" // Total
+		"\x00" // Offset
+		"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16" // Feature 1
+		"\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32" // Feature 2
+		"\x33\x34\x35\x36\x37\x38\x39\x40\x41\x42\x43\x44\x45\x46\x47\x48" // Feature 3
+		;
+		if(memcmp(ref, payload, length) != 0) {
+			err1("payload mismatch");
+			test_errors++;
+		}
+	}
+
+	if(packets_sent == 3) {
+		uint8_t ref[] =
+		"\x02\x02" // hdr-version
+		"\x88\x77\x66\x55\x44\x33\x22\x11" // EUI64
+		"\x00\x00\x00\x01" // boot_number
+		"\x03" // Total
+		"\x01" // Offset
+		"\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32" // Feature 2
+		"\x33\x34\x35\x36\x37\x38\x39\x40\x41\x42\x43\x44\x45\x46\x47\x48" // Feature 3
+		;
+		if(memcmp(ref, payload, length) != 0) {
+			err1("payload mismatch");
+			test_errors++;
+		}
+	}
 
 	if(_sdf1 == NULL) {
 		_msg1 = msg;
@@ -292,7 +332,7 @@ int testListFeaturesResponse() {
 	device_feature_t dftrs[3];
 	devf_add_feature(&dftrs[0], (nx_uuid_t*)"\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16");
 	devf_add_feature(&dftrs[1], (nx_uuid_t*)"\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32");
-	//devf_add_feature(&dftrs[2], (nx_uuid_t*)"\x33\x34\x35\x36\x37\x38\x39\x40\x41\x42\x43\x44\x45\x46\x47\x48");
+	devf_add_feature(&dftrs[2], (nx_uuid_t*)"\x33\x34\x35\x36\x37\x38\x39\x40\x41\x42\x43\x44\x45\x46\x47\x48");
 
 	device_announcer_t announcer;
 	deva_init();
@@ -304,6 +344,16 @@ int testListFeaturesResponse() {
 		if(_sdf1 != NULL) {
 			_sdf1(radio, _msg1, COMMS_SUCCESS, _user1);
 			_sdf1 = NULL;
+		}
+		if(i == 5) {
+			comms_msg_t msg;
+			comms_init_message(radio, &msg);
+			comms_set_packet_type(radio, &msg, 0xDA);
+			memcpy(comms_get_payload(radio, &msg, 2), "\x10\x02", 2);
+			comms_set_payload_length(radio, &msg, 2);
+			comms_am_set_destination(radio, &msg, 0xFFFF);
+			comms_am_set_source(radio, &msg, 0x1234);
+			comms_deliver(radio, &msg);
 		}
 		if(i == 10) {
 			comms_msg_t msg;
@@ -327,8 +377,8 @@ int testListFeaturesResponse() {
 		}
 	}
 
-	if(packets_sent != 2) {
-		err1("testDescriptionResponse - packet count: %d != %d", packets_sent, 2);
+	if(packets_sent != 3) {
+		err1("testDescriptionResponse - packet count: %d != %d", packets_sent, 3);
 		return 1;
 	}
 	if(test_errors > 0) {
